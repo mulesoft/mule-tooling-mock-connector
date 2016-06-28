f6c5684e2bfef608f041036814094a59fb1e6638
pablocabrera85 <pablo.cabrera@mulesoft.com>
2016-06-28 15:22:52 -0300

    Add configurable at provider

--- extension-with-operations-and-sources-by-config/src/main/java/org/mule/modules/basic/SourceConnectionProvider.java
+++ extension-with-operations-and-sources-by-config/src/main/java/org/mule/modules/basic/SourceConnectionProvider.java
@@ -6,10 +6,17 @@ import org.mule.runtime.api.connection.ConnectionHandlingStrategyFactory;
 import org.mule.runtime.api.connection.ConnectionProvider;
 import org.mule.runtime.api.connection.ConnectionValidationResult;
 import org.mule.runtime.extension.api.annotation.Alias;
+import org.mule.runtime.extension.api.annotation.Parameter;
 
 @Alias("source-connection")
 public class SourceConnectionProvider implements ConnectionProvider<SourceBasicExtensionConfig,DummyConnection>
 {
+
+    /**
+     * Provider parameter description
+     */
+    @Parameter
+    private String sourceProviderParameter;
 
     @Override
     public DummyConnection connect(SourceBasicExtensionConfig sourceBasicExtensionConfig) throws ConnectionException
