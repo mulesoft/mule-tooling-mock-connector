b7c9171584b584c869e7302ba8360656666354ff
pablocabrera85 <pablo.cabrera@mulesoft.com>
2016-08-30 18:39:39 -0300

    Add Extension with POJO and ExpressionSupport.REQUIRED

---
+++ extension-with-pojo-expression-required/src/main/java/org/mule/modules/basic/BasicConnectionProvider.java
@@ -0,0 +1,46 @@
+package org.mule.modules.basic;
+
+import static org.mule.runtime.extension.api.introspection.parameter.ExpressionSupport.REQUIRED;
+import org.mule.modules.basic.model.ComplexPojo;
+import org.mule.modules.basic.model.Ingredient;
+import org.mule.runtime.api.connection.ConnectionException;
+import org.mule.runtime.api.connection.ConnectionProvider;
+import org.mule.runtime.api.connection.ConnectionValidationResult;
+import org.mule.runtime.extension.api.annotation.Expression;
+import org.mule.runtime.extension.api.annotation.Parameter;
+
+/**
+ * Connection Provider description
+ */
+public class BasicConnectionProvider implements ConnectionProvider<DummyConnection> {
+
+	/**
+	 * Complex Pojo At Provider description
+	 */
+	@Parameter
+	@Expression(REQUIRED)
+	private ComplexPojo complexPojoAtProvider;
+
+	/**
+	 * Ingredient At Provider description
+	 */
+	@Parameter
+	@Expression(REQUIRED)
+	private Ingredient ingredientAtProvider;
+
+	@Override
+	public DummyConnection connect() throws ConnectionException {
+		return null;
+	}
+
+	@Override
+	public void disconnect(DummyConnection connection) {
+
+	}
+
+	@Override
+	public ConnectionValidationResult validate(DummyConnection connection) {
+		return null;
+	}
+
+}
---
+++ extension-with-pojo-expression-required/src/main/java/org/mule/modules/basic/BasicOperations.java
@@ -0,0 +1,20 @@
+package org.mule.modules.basic;
+
+import static org.mule.runtime.extension.api.introspection.parameter.ExpressionSupport.REQUIRED;
+import org.mule.modules.basic.model.ComplexPojo;
+import org.mule.modules.basic.model.Ingredient;
+import org.mule.runtime.extension.api.annotation.Expression;
+
+public class BasicOperations {
+
+	/**
+	 * Operation With String description
+	 *
+	 * @param complexPojoAtOperation Complex Pojo at operation description
+	 * @param ingredientAtOperation Ingredient at operation description
+	 */
+	public String operationWithPojo(@Expression(REQUIRED) ComplexPojo complexPojoAtOperation, @Expression(REQUIRED) Ingredient ingredientAtOperation) {
+			return null;
+	}
+
+}
