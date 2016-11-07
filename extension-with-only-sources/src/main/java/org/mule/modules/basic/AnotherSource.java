1011c80054001cbf8857e18d8305579cd126974d
pablocabrera85 <pcabrera@mulesoft.com>
2016-11-07 14:41:42 -0300

    Add on error and on success methods to source

--- extension-with-only-sources/src/main/java/org/mule/modules/basic/AnotherSource.java
+++ extension-with-only-sources/src/main/java/org/mule/modules/basic/AnotherSource.java
@@ -1,6 +1,8 @@
 package org.mule.modules.basic;
 
 import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.extension.api.annotation.execution.OnError;
+import org.mule.runtime.extension.api.annotation.execution.OnSuccess;
 import org.mule.runtime.extension.api.annotation.param.Parameter;
 import org.mule.runtime.extension.api.annotation.param.UseConfig;
 import org.mule.runtime.extension.api.runtime.source.Source;
@@ -28,6 +30,16 @@ public class AnotherSource extends Source<String,BasicAttributes>
     @Override
     public void onStop()
     {
+
+    }
+
+    @OnSuccess
+    public void logSuccess( String message){
+
+    }
+
+    @OnError
+    public void logError( String message, String details){
 
     }
 }
