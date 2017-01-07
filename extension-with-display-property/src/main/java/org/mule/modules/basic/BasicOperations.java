6e17e985ffaf68139b84642d80b35a478748322c
pablocabrera85 <pcabrera@mulesoft.com>
2017-01-07 17:00:03 -0300

    Add @Example for testing

--- extension-with-display-property/src/main/java/org/mule/modules/basic/BasicOperations.java
+++ extension-with-display-property/src/main/java/org/mule/modules/basic/BasicOperations.java
@@ -3,6 +3,7 @@ package org.mule.modules.basic;
 import org.mule.modules.basic.model.SOSPojo;
 import org.mule.runtime.extension.api.annotation.param.UseConfig;
 import org.mule.runtime.extension.api.annotation.param.display.DisplayName;
+import org.mule.runtime.extension.api.annotation.param.display.Example;
 
 public class BasicOperations
 {
@@ -14,7 +15,7 @@ public class BasicOperations
      * @param param  String param description
      */
     @DisplayName("Custom operation label")
-    public String operationWithString(@UseConfig BasicExtensionConfig config, @DisplayName("Foo Operation Parameter") String param, @DisplayName("SOS") SOSPojo pojoParam)
+    public String operationWithString(@UseConfig BasicExtensionConfig config, @DisplayName("Foo Operation Parameter") String param, @DisplayName("SOS") SOSPojo pojoParam, @Example("Example hint") String paramWithExample)
     {
         return param;
     }
