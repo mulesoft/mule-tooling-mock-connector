078ec775acdb1140078f956a4d9891c3d9651efe
pablocabrera85 <pcabrera@mulesoft.com>
2017-01-19 14:35:27 -0300

    Add extension to test dependencies

---
+++ extension-with-dep2/src/main/java/org/mule/modules/basic/BasicExtension.java
@@ -0,0 +1,13 @@
+package org.mule.modules.basic;
+
+import org.mule.runtime.extension.api.annotation.Extension;
+import org.mule.runtime.extension.api.annotation.Operations;
+
+/**
+ * Extension javadoc description
+ */
+@Extension(name = "BasicDep1", description = "Basic Connector Description")
+@Operations({ BasicOperations.class })
+public class BasicExtension {
+
+}
---
+++ extension-with-dep3/src/main/java/org/mule/modules/basic/BasicOperations.java
@@ -0,0 +1,14 @@
+package org.mule.modules.basic;
+
+public class BasicOperations {
+
+	/**
+	 * Operation With String description
+	 *
+	 * @param param String param description
+	 */
+	public String operationWithString(String param) {
+			return param;
+	}
+
+}
