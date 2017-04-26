d24b3150220b010c9fccedaf3caa4dab1e26e0c3
pablocabrera85 <pcabrera@mulesoft.com>
2017-04-26 14:48:39 -0300

    Add more complex @Placement example

--- extension-with-placement-property/src/main/java/org/mule/modules/basic/AnotherParameterGroup.java
+++ extension-with-placement-property/src/main/java/org/mule/modules/basic/AnotherParameterGroup.java
@@ -1,14 +1,26 @@
 package org.mule.modules.basic;
 
 import org.mule.runtime.extension.api.annotation.param.Parameter;
+import org.mule.runtime.extension.api.annotation.param.display.Placement;
 
 /**
  * Created by pablocabrera on 11/29/16.
  */
 public class AnotherParameterGroup
 {
+    //Fields with no order will be shown after the ones that have an order
     @Parameter
+    private Integer noOrderField;
+
+    //Since placement is highier, the field will be shown second
+    @Parameter
+    @Placement(order=3)
+    private Integer advanceSecondField;
+
+    @Parameter
+    @Placement(order=1)
     private Integer advanceIntField;
+
 
     public Integer getAdvanceIntField()
     {
@@ -18,5 +30,15 @@ public class AnotherParameterGroup
     public void setAdvanceIntField(Integer advanceIntField)
     {
         this.advanceIntField = advanceIntField;
+    }
+
+    public Integer getNoOrderField()
+    {
+        return noOrderField;
+    }
+
+    public Integer getAdvanceSecondField()
+    {
+        return advanceSecondField;
     }
 }
