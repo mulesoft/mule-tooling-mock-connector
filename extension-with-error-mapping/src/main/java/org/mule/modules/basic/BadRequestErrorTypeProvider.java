8ece9dc752f28b4fbbfe89f6d90e58351e3e3aac
pablocabrera85 <pablo.cabrera@mulesoft.com>
2017-07-11 16:06:06 -0300

    Remove guava immutable set in favor of java.util.Collections

--- extension-with-error-mapping/src/main/java/org/mule/modules/basic/BadRequestErrorTypeProvider.java
+++ extension-with-error-mapping/src/main/java/org/mule/modules/basic/BadRequestErrorTypeProvider.java
@@ -4,8 +4,7 @@ import static org.mule.modules.basic.MyErrors.BAD_REQUEST;
 import org.mule.runtime.extension.api.annotation.error.ErrorTypeProvider;
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
 
-import com.google.common.collect.ImmutableSet;
-
+import java.util.Collections;
 import java.util.Set;
 
 public class BadRequestErrorTypeProvider implements ErrorTypeProvider
@@ -14,8 +13,6 @@ public class BadRequestErrorTypeProvider implements ErrorTypeProvider
     @Override
     public Set<ErrorTypeDefinition> getErrorTypes()
     {
-        return ImmutableSet.<ErrorTypeDefinition>builder()
-                .add(BAD_REQUEST)
-                .build();
+        return Collections.singleton(BAD_REQUEST);
     }
 }
