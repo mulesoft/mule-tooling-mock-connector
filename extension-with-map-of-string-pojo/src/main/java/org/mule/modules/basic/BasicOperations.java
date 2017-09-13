3eb84c83de63caea162fccdafc71d1fee1d96a36
pablocabrera85 <pablo.cabrera@mulesoft.com>
2017-09-13 14:38:17 -0300

    Add scenario of Map<String,Pojo> with no DSL for the value type

--- extension-with-map-of-string-pojo/src/main/java/org/mule/modules/basic/BasicOperations.java
+++ extension-with-map-of-string-pojo/src/main/java/org/mule/modules/basic/BasicOperations.java
@@ -1,19 +1,29 @@
 package org.mule.modules.basic;
 
 import org.mule.modules.basic.model.Ingredient;
+import org.mule.modules.basic.model.NotTopLevelPojo;
 import org.mule.runtime.extension.api.annotation.param.Config;
+import org.mule.runtime.extension.api.annotation.param.Optional;
 
 import java.util.Map;
 
-public class BasicOperations {
+public class BasicOperations
+{
 
-	/**
-	 * Operation With Map description
-	 *
-	 * @param mapParameters Map param description
-	 */
-	public Ingredient operationWithMap(@Config BasicExtensionConfig config, Map<String,Ingredient>  mapParameters) {
-		return null;
-	}
+    /**
+     * Operation With Map description
+     *
+     * @param mapParameters Map param description
+     */
+    public Ingredient operationWithMap(@Config BasicExtensionConfig config, Map<String, Ingredient> mapParameters)
+    {
+        return null;
+    }
+
+    public NotTopLevelPojo operationWithMapOfNotTopLevelPojo(@Config BasicExtensionConfig config, @Optional(defaultValue = "#[payload]") Map<String, NotTopLevelPojo> mapParam)
+    {
+        return null;
+    }
+
 
 }
